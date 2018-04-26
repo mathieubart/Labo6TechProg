ac9f741bb5ee7839478abe2cb75ecf83405a0d3e
mathieubart <mathieu_fournier99@hotmail.fr>
2018-04-26 18:38:06 -0400

    Main function first push

--- Matermind/Matermind/Matermind.cpp
+++ Matermind/Matermind/Matermind.cpp
@@ -1,11 +1,33 @@
-// Matermind.cpp : Définit le point d'entrée pour l'application console.
-//
 
 #include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <random>
 
+using namespace std;
 
 int main()
 {
-    return 0;
+	cout << "Instructions" << endl;
+
+	vector<vector<char>> plaquette = vector<vector<char>>();
+	vector<vector<char>> pions = vector<vector<char>>();
+	vector<char> couleurs = vector<char>({ 'R', 'B', 'J', 'V', 'O' });
+
+	int tour = 1;
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			plaquette[i][j] = ' ';
+		}
+		for (int j = 0; j < 2; j++)
+		{
+			pions[i][j] = ' ';
+		}
+	}
+
+
 }
 
